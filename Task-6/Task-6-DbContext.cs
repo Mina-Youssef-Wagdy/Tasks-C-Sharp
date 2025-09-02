67c219340151cff1eec06b543b119604d3f80929
Mina-Youssef-Wagdy <minayossef222@gmail.com>
2025-09-03 00:41:05 +0300

    .

--- Task-6/Task-6-DbContext.cs
+++ Task-6/Task-6-DbContext.cs
@@ -1,35 +1,41 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task_6
+
 {
-    public class Task_6_DbContext : DbContext
+
+    public class Task_6_DbContext:DbContext
     {
 
-        public Task_6_DbContext(DbContextOptions<Task_6_DbContext> Options):base(Options) 
+
+        public Task_6_DbContext(DbContextOptions<Task_6_DbContext> Options) : base(Options)
         {
-            
+
         }
         public DbSet<Employee> Employees { set; get; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        public class Employee
         {
-            modelBuilder.Entity<Employee>(entity =>
-            {
-                entity.Property(e => e.Id).HasColumnName("ID");
-                entity.Property(e => e.Job).HasMaxLength(50);
-                entity.Property(e => e.Name).HasMaxLength(50);
-                entity.Property(e => e.Salary).HasColumnType("decimal(18,2)").IsRequired();
+            [Key]
+            public int Id { get; set; }
+            public string Name { get; set; }
+            [Required, MaxLength(50)]
+            public string Job { get; set; }
+            [Required]
+            public double Salary { get; set; }
+            [Required]
 
-            });
+            public byte[] Photo { get; set; }
+
+
+
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=ADMINISTRATOR\\SQLEXPRESS01;Initial Catalog=Task_6_DB;Integrated Security=True;Trust Server Certificate=True");
-        }
     }
 }
+
